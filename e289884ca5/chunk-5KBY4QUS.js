ec1c3fd44d2bb2bb3e6e93f3b788235f7440a6ff
dgo <dgo@arkanium.fr>
2024-03-15 05:57:03 +0100

    release for e289884ca5

---
+++ e289884ca5/chunk-5KBY4QUS.js
@@ -0,0 +1,1 @@
+import"./chunk-YEG3NED5.js";var o={description:"\xA1Correcto! \xBFPero, por qu\xE9?",answer:" esta embarazada",continue:"Continuar"};export{o as lang};
--- e289884ca5/chunk-BC4HP5ZZ.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-YEG3NED5.js";var o={description:"\xA1Correcto! \xBFPero, por qu\xE9?",answer:" est\xE0 embarazada",continue:"Continuar"};export{o as lang};
